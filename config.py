98707d31c12bbf126d64ef4c732decbe0ee1b472
Alex-gikungu <alexigikungu.012@gmail.com>
2023-10-01 22:39:47 +0300

    added config file

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///heros.db'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///heros.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
