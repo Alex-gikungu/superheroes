9e20ccd9ede889ee047c25b709c4e07b05643d91
Alex-gikungu <alexigikungu.012@gmail.com>
2023-10-01 22:40:57 +0300

    added model file it contains three classes that contais tables and their relationships

--- models.py
+++ models.py
@@ -1,25 +1,25 @@
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
-# class Hero(db.Model):
-#     __tablename__ = 'hero'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     super_name = db.Column(db.String(100), nullable=False)
-#     powers = db.relationship("Hero_powers", backref="hero")
+class Hero(db.Model):
+    __tablename__ = 'hero'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    super_name = db.Column(db.String(100), nullable=False)
+    powers = db.relationship("Hero_powers", backref="hero")
 
-# class Hero_powers(db.Model):
-#     __tablename__ = 'hero_powers'
-#     id = db.Column(db.Integer, primary_key=True)
-#     strength = db.Column(db.String(100))
-#     hero_id = db.Column(db.Integer, db.ForeignKey("hero.id"))
-#     power_id = db.Column(db.Integer, db.ForeignKey("powers.id"))
+class Hero_powers(db.Model):
+    __tablename__ = 'hero_powers'
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(100))
+    hero_id = db.Column(db.Integer, db.ForeignKey("hero.id"))
+    power_id = db.Column(db.Integer, db.ForeignKey("powers.id"))
 
-# class Powers(db.Model):
-#     __tablename__ = 'powers'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     description = db.Column(db.String(100), nullable=False)
-#     heroes = db.relationship("Hero_powers", backref="power")
+class Powers(db.Model):
+    __tablename__ = 'powers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    heroes = db.relationship("Hero_powers", backref="power")
 
