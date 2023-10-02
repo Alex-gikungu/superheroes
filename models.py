b0a3cee3d624333722980ed5c1d1ae6e0304e008
Alex-gikungu <alexigikungu.012@gmail.com>
2023-10-02 11:41:50 +0300

    added validations

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -7,7 +9,16 @@ class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     super_name = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
     powers = db.relationship("Hero_powers", backref="hero")
+
+    @validates('name', 'super_name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError(f"'{key}' cannot be empty.")
+        return value
 
 class Hero_powers(db.Model):
     __tablename__ = 'hero_powers'
@@ -15,11 +26,27 @@ class Hero_powers(db.Model):
     strength = db.Column(db.String(100))
     hero_id = db.Column(db.Integer, db.ForeignKey("hero.id"))
     power_id = db.Column(db.Integer, db.ForeignKey("powers.id"))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    @validates('strength')
+    def validate_strength(self, key, value):
+        if not value:
+            raise ValueError("'strength' cannot be empty.")
+        return value
 
 class Powers(db.Model):
     __tablename__ = 'powers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
     heroes = db.relationship("Hero_powers", backref="power")
 
+    @validates('name', 'description')
+    def validate_name_description(self, key, value):
+        if not value:
+            raise ValueError(f"'{key}' cannot be empty.")
+        return value
